7c600d8699ff445fc4137d0bd54ef2825e397e83
https://github.com/abdullayev96 <buymax586@gmail.com>
2022-12-07 17:39:45 +0500

    good

--- cites/cites/settings.py
+++ cites/cites/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yjv8i*d4u@$9)$4(+xe@(^u4+bw1$sr%fgabh!&+@*!9k_v1j='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
